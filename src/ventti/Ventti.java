717a3496610969fe8ee743182f231589e6a2efbd
Pinja <pinskumelissa@gmail.com>
2018-10-01 13:27:42 +0300

    kommentti

--- src/ventti/Ventti.java
+++ src/ventti/Ventti.java
@@ -17,6 +17,7 @@ public class Ventti {
     public static void main(String[] args) {
         // TODO code application logic here
         // Hello world :)
+        //kommentti
     }
     //testitesti
 }
