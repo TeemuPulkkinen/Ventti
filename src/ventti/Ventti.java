c5a292f078413472f4baef1626f2fceadeaee525
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2018-10-03 13:45:35 +0300

    Edistystä

--- src/ventti/Ventti.java
+++ src/ventti/Ventti.java
@@ -5,8 +5,7 @@
  */
 package ventti;
 
-//importteja
-import java.util.ArrayList;
+
 
 /**
  *
@@ -17,7 +16,7 @@ public class Ventti {
     public static void main(String[] args) {
         int[] korttimaara = new int[52];
         String[] maat = {"Pata", "Hertta", "Ruutu", "Risti"};
-        String[] arvot = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jätkä", "Akka", "Ukko", "Ässä"};
+        String[] arvot = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "jätkä", "akka", "kurko", "ässä"};
 
         // Initialize cards
         for (int i = 0; i < korttimaara.length; i++) {
@@ -32,10 +31,22 @@ public class Ventti {
             korttimaara[index] = temp;
         }
 
-        /* Display the all the cards
+        // Display the all the cards
             for (int i = 0; i < 52; i++) {
                 String maa = maat[korttimaara[i] / 13];
                 String arvo = arvot[korttimaara[i] % 13];
-                System.out.println(arvo + " " + maa */
+                System.out.println(arvo + "" + maa);
+            }        
+        
+        System.out.println("Terve tuloa pelaamaan Teemun ja Pinjan eeppistä venttipeliä!");
+        System.out.println("Tässä versiossa emäntä voittaa tasapelit ja ässän arvo on 14.");
+        
+        int pelaajankasi = 0;
+        int emannankasi = 0;
+        
+        
+        
+            
+        }
     }
-}
+
