da2b63ab3aaa4ee39c47cd06b53fce4b09865c5a
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2018-10-05 12:59:31 +0300

    Uusin versio

--- src/ventti/Ventti.java
+++ src/ventti/Ventti.java
@@ -20,13 +20,14 @@ public class Ventti {
     public static void main(String[] args) {
         // luodaan pakka
         int[] korttimaara = new int[52];
-        Integer[] arvot = new Integer[]{2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14};
+        Integer[] arvot = new Integer[]{2, 2, 2, 2, 3, 3, 3, 3,  4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14};
 
         //peli käynnistyy
         System.out.println("Terve tuloa pelaamaan Teemun ja Pinjan eeppistä venttipeliä!");
         System.out.println("Tässä versiossa emäntä voittaa tasapelit ja ässän arvo on 14.");
         //luodaan käsimuuttujat
         int pelaajanKasi = 0;
+        int pelaajanKortit = 0;
         int emannanKasi = 0;
         //pelaajan vastauksen lukemista varten
         String luettu;
@@ -36,6 +37,7 @@ public class Ventti {
         //nostetaan ensimmäinen pelaajan kortti
         Random rand = new Random();
         int element = l.get(rand.nextInt(l.size()));
+        pelaajanKortit += 1;
 
         //lisätään pelaajan ensimmäinen kortti käteen
         pelaajanKasi += element;
@@ -49,6 +51,8 @@ public class Ventti {
             element = l.get(rand.nextInt(l.size()));
             System.out.println("Vedetty kortti on " + element + ".");
             pelaajanKasi += element;
+            pelaajanKortit += 1;
+            System.out.println("Olet nostanut " + pelaajanKortit + " korttia.");
             System.out.println("Kätesi summa on yhteensä " + pelaajanKasi + ". Haluatko vetää uuden kortin? (joo/ei)");
             luettu = lukija.nextLine();
         }
@@ -63,17 +67,16 @@ public class Ventti {
                 emannanKasi += element;
                 System.out.println("Emäntä nostaa uuden kortin.");
                 System.out.println("Emännän käden summa on " + emannanKasi + ".");
-                System.out.println("Tämä on kivaa");
             }
 
         }
-
+        
         if (pelaajanKasi > 21) {
             System.out.println("Hävisit!");
         } else if (emannanKasi <= pelaajanKasi && emannanKasi <= 21) {
             System.out.println("Emäntä voitti!");
-            //mikäli pelaajan käsi on pienempi tai yhtäsuuri kuin 21 niin hän voittaa
-        } else {
+            //mikäli pelaajan käsi on pienempi tai yhtäsuuri kuin 21 tai kädessä on 5 korttia niin hän voittaa
+        } else if (pelaajanKasi <=21 || pelaajanKortit == 5) {
             System.out.println("Voitit!");
 
         }
