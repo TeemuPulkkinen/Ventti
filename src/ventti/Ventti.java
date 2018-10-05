b57f09bfd37dc671eee37f369531449a12b9e815
Teemu Pulkkinen <pulkkinen.teemu@gmail.com>
2018-10-05 13:14:55 +0300

    Musaa :D

--- src/ventti/Ventti.java
+++ src/ventti/Ventti.java
@@ -5,11 +5,15 @@
  */
 package ventti;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 /**
  *
@@ -20,11 +24,16 @@ public class Ventti {
     public static void main(String[] args) {
         // luodaan pakka
         int[] korttimaara = new int[52];
-        Integer[] arvot = new Integer[]{2, 2, 2, 2, 3, 3, 3, 3,  4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14};
-
+        Integer[] arvot = new Integer[]{2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 14};
+        final JFXPanel fxPanel = new JFXPanel();
         //peli käynnistyy
         System.out.println("Terve tuloa pelaamaan Teemun ja Pinjan eeppistä venttipeliä!");
         System.out.println("Tässä versiossa emäntä voittaa tasapelit ja ässän arvo on 14.");
+        String bip = "Bio_UnitMetre_-_03_-_SciFi.mp3";
+        Media hit = new Media(new File(bip).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(hit);
+        mediaPlayer.play();
+
         //luodaan käsimuuttujat
         int pelaajanKasi = 0;
         int pelaajanKortit = 0;
@@ -70,15 +79,14 @@ public class Ventti {
             }
 
         }
-        
+
         if (pelaajanKasi > 21) {
             System.out.println("Hävisit!");
         } else if (emannanKasi <= pelaajanKasi && emannanKasi <= 21) {
             System.out.println("Emäntä voitti!");
             //mikäli pelaajan käsi on pienempi tai yhtäsuuri kuin 21 tai kädessä on 5 korttia niin hän voittaa
-        } else if (pelaajanKasi <=21 || pelaajanKortit == 5) {
+        } else if (pelaajanKasi <= 21 || pelaajanKortit == 5) {
             System.out.println("Voitit!");
-
         }
     }
 }
